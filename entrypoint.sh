ccc12b80d6ac46fa8bcfcbabedee87593129ff35
hbutcn <112219197+hbutcn@users.noreply.github.com>
2023-03-21 17:24:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 # 设置各变量
 WP=${WP:-'crgo'}
 UUID=${UUID:-'be04add9-5c68-8bab-950c-08cd5320df18'}
-URL=${SERVER_URL:8}
+#URL=${SERVER_URL:8}
+URL=${APP_NAME+'.b4a.run'}
 EXEC=$(echo $RANDOM | md5sum | head -c 6; echo)
 generate_config() {
   cat > config.json << EOF
